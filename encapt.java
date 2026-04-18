71af74f9e47267b657bf14a75d46948be2022ba0
jashanpreet <jashanpreetkaurkaur54@gmail.com>
2026-04-18 13:23:40 +0530

    encapt

---
+++ encapt.java
@@ -0,0 +1,28 @@
+class Locker {
+ private int pin = 12345;
+ private boolean isOpen = false;
+public void openLocker(int enteredPin) {
+if (enteredPin == pin) {
+isOpen = true;
+System.out.println("Locker opened");
+ } else {
+System.out.println("Wrong pin");
+ }
+}
+ public void checkStatus() {
+if (isOpen) {
+System.out.println("Locker is open");
+} else {
+System.out.println("Locker is closed");
+ }
+ }
+}
+public class encapt {
+    public static void main(String[] args) {
+    Locker A = new Locker();
+    A.checkStatus();        
+    A.openLocker(1111);     
+    A.openLocker(12345);     
+    A.checkStatus();       
+    }
+}
