053990853424c1be33ef05236b019f29d229e738
jashanpreet <jashanpreetkaurkaur54@gmail.com>
2026-04-25 12:32:04 +0530

    Bitwise

---
+++ Bitwise.java
@@ -0,0 +1,11 @@
+class Bitwise {
+public static void main(String[] args) {
+int a = 9;
+int b = 10;
+System.out.println(a&b);
+System.out.println(a|b);
+System.out.println(a^b);
+System.out.println(a<<1);
+
+}
+}
