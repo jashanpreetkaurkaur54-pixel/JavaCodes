65aec078bc1a99cacd985458dc4143bae0458621
jashanpreet <jashanpreetkaurkaur54@gmail.com>
2026-04-27 10:20:13 +0530

    grades

---
+++ grades.java
@@ -0,0 +1,14 @@
+public class grades{
+    public static void main(String[] args) {
+int marks= 56;
+     if (marks >= 90){
+         System.out.println("A");
+      } else if  (marks >= 75){
+            System.out.println("B");
+      }else if (marks >= 50){
+            System.out.println("C");
+       } else{
+      System.out.println("Fail");
+    }
+}
+}
