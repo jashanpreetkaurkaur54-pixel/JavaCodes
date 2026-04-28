1be9386b22f73e1bfd5689f838c057bfeeee046e
jashanpreet <jashanpreetkaurkaur54@gmail.com>
2026-04-28 21:43:25 +0530

    MaximumElemantMatrix

---
+++ MaximumElemantMatrix.java
@@ -0,0 +1,18 @@
+public class MaximumElemantMatrix {
+ public static void main(String[] args) {
+ int arr[][] = {
+{1, 9, 3},
+{4, 15, 6},
+{7, 8, 2}
+ };
+int max = arr[0][0]; 
+for(int i = 0; i < arr.length; i++) {
+for(int j = 0; j < arr[i].length; j++) {
+ if(arr[i][j] > max) {
+ max = arr[i][j];
+    }
+ }
+ }
+ System.out.println("Maximum Element = " + max);
+    }
+}
