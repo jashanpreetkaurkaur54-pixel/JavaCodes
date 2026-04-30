9ec55d3db698257735198988c8851101d7564f12
jashanpreet <jashanpreetkaurkaur54@gmail.com>
2026-04-30 11:12:48 +0530

    PositiveNegativeZero

---
+++ PositiveNegativeZero.java
@@ -0,0 +1,13 @@
+public class PositiveNegativeZero {
+public static void main(String[] args){
+int n = 5 ;
+if(n>0){
+System.out.println("positive number");
+}else if (n<0){
+System.out.println("negative  number");
+}
+else{
+System.out.println("negative  number"); 
+}
+}
+}
