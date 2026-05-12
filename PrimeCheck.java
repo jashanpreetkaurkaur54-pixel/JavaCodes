23cf0432aa9e9991bf55518c480f1230f6bed2a3
jashanpreet <jashanpreetkaurkaur54@gmail.com>
2026-05-12 13:54:04 +0530

    InterestRate

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -1,5 +1,5 @@
 public class PrimeCheck {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         int num = 11;   
         boolean isPrime = true;
  if (num <= 1) {
@@ -9,13 +9,13 @@ for (int i = 2; i <= Math.sqrt(num); i++) {
 if (num % i == 0) {
 isPrime = false;
 break;
-                }
-            }
-        }
+  }
+  }
+  }
 if (isPrime) {
  System.out.println(num + " is a Prime Number");
         } else {
       System.out.println(num + " is NOT a Prime Number");
-        }
+     }
     }
 }
